75dfeb6dd9e8736586a2a32bf417cf9a51c2d4e7
kangtae49 <kangtae49@gmail.com>
2021-01-07 17:14:18 +0900

    env

--- src/fbase.js
+++ src/fbase.js
@@ -2,12 +2,12 @@
 import firebase from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 firebase.initializeApp(firebaseConfig);
 
